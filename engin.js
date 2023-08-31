33e3bf36dad244d527f60deb3f84d51be6f723f7
Masum-Reza <careerhunter4280@gmail.com>
2023-08-30 21:47:19 -0700

    completed sessional task

--- engin.js
+++ engin.js
@@ -14,10 +14,10 @@ allNewsCat()
 // loading handler
 let loadingHandler = (isLoading) => {
     let loadingSpinner = document.getElementById('loadingSpinner');
-    if(isLoading){
+    if (isLoading) {
         loadingSpinner.classList.remove('hidden')
     }
-    else{
+    else {
         loadingSpinner.classList.add('hidden')
     }
 }
@@ -27,7 +27,7 @@ let loadingHandler = (isLoading) => {
 let showAllCat = (allCat) => {
     let newsCategories = document.getElementById('newsCategories');
 
-    
+
     allCat.forEach(category => {
         // console.log(category)
         let p = document.createElement('p');
@@ -54,39 +54,44 @@ let catNewses = async (id) => {
 
     categoryNews.textContent = '';
 
-    partNews.forEach(news => {
-        console.log(news)
-        let div = document.createElement('div');
-        div.classList = 'card bg-base-100 shadow-xl font-semibold'
-        div.innerHTML = `
-        <figure><img src="${news?.image_url}" /></figure>
-            <div class="card-body">
-                <h2 class="card-title h-24"> ${news?.title} </h2>
-
-                <div class="flex justify-between text-sm">
-                    <div class="space-y-1">
-                        <h3>${news?.author?.name || 'Global'}</h3>
-                        <p>${news?.author?.published_date || 'Not found'}</p>
-                        <img class="w-1/3 lg:w-1/4 rounded-full"
-                            src="${news?.author?.img || 'Not found'}
-                            alt="">
+    if (partNews.length > 0) {
+        partNews.forEach(news => {
+            
+            let div = document.createElement('div');
+            div.classList = 'card bg-base-100 shadow-xl font-semibold'
+            div.innerHTML = `
+            <figure><img src="${news?.image_url}" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title h-24"> ${news?.title} </h2>
+    
+                    <div class="flex justify-between text-sm">
+                        <div class="space-y-1">
+                            <h3>${news?.author?.name || 'Global'}</h3>
+                            <p>${news?.author?.published_date || 'Not found'}</p>
+                            <img class="w-1/3 lg:w-1/4 rounded-full"
+                                src="${news?.author?.img || 'Not found'}
+                                alt="">
+                        </div>
+    
+                        <div>
+                            <p>${news?.others_info?.is_trending ? 'tranding' : 'Not in Trand'}</p>
+                            <p>${news?.rating?.number || 'Unavailable'}</p>
+                            <p>${news?.rating?.badge || 'Unavilable'}</p>
+                            <p>View:${news?.total_view}</p>
+                        </div>
+    
                     </div>
-
-                    <div>
-                        <p>${news?.others_info?.is_trending? 'tranding' : 'Not in Trand'}</p>
-                        <p>${news?.rating?.number || 'Unavailable'}</p>
-                        <p>${news?.rating?.badge || 'Unavilable'}</p>
-                        <p>View:${news?.total_view}</p>
-                    </div>
-
+    
+                    <button class="btn shadow-md shadow-gray-300">Details</button>
+    
                 </div>
+            `
+            categoryNews.appendChild(div)
 
-                <button class="btn shadow-md shadow-gray-300">Details</button>
-
-            </div>
-        `
-        categoryNews.appendChild(div)
-    });
+        })
+    } else {
+        categoryNews.innerText = 'No News Found'
+    }
 
     loadingHandler(false)
 
@@ -94,3 +99,8 @@ let catNewses = async (id) => {
 
 // by default showing all news
 catNewses('08')
+
+// modalHandler
+function modalHandler (){
+    console.log(event.target.parentNode.innerText)
+}
