fc690752c55df71b04c8ee81678f1c7ea4dada70
Masum-Reza <careerhunter4280@gmail.com>
2023-08-30 18:10:25 -0700

    showing individual news of clicking

--- engin.js
+++ engin.js
@@ -15,11 +15,62 @@ let showAllCat = (allCat) => {
 
 
     allCat.forEach(category => {
-        console.log(category)
+        // console.log(category)
         let p = document.createElement('p');
-        p.classList = 'cursor-pointer  hover:-translate-y-[0.12rem] active:translate-y-[0.12rem]'
+        p.classList = 'cursor-pointer  hover:-translate-y-[0.12rem] active:translate-y-[0.12rem] shadow-sm shadow-black'
         p.innerText = category.category_name;
         newsCategories.appendChild(p);
+
+        p.addEventListener('click', () => {
+            catNewses(category.category_id)
+        })
+    });
+
+}
+
+
+let catNewses = async (id) => {
+    let response = await fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
+    let info = await response.json();
+    let partNews = info.data;
+    // console.log(partNews)
+
+    let categoryNews = document.getElementById('categoryNews');
+
+    categoryNews.textContent = '';
+
+    partNews.forEach(news => {
+        console.log(news)
+        let div = document.createElement('div');
+        div.classList = 'card bg-base-100 shadow-xl font-semibold'
+        div.innerHTML = `
+        <figure><img src="${news?.image_url}" /></figure>
+            <div class="card-body">
+                <h2 class="card-title h-24"> ${news?.title} </h2>
+
+                <div class="flex justify-between text-sm">
+                    <div class="space-y-1">
+                        <h3>${news?.author?.name || 'Global'}</h3>
+                        <p>${news?.author?.published_date || 'Not found'}</p>
+                        <img class="w-1/3 lg:w-1/4 rounded-full"
+                            src="${news?.author?.img || 'Not found'}
+                            alt="">
+                    </div>
+
+                    <div>
+                        <p>${news?.others_info?.is_trending? 'tranding' : 'Not in Trand'}</p>
+                        <p>${news?.rating?.number || 'Unavailable'}</p>
+                        <p>${news?.rating?.badge || 'Unavilable'}</p>
+                        <p>View:${news?.total_view}</p>
+                    </div>
+
+                </div>
+
+                <button class="btn shadow-md shadow-gray-300">Details</button>
+
+            </div>
+        `
+        categoryNews.appendChild(div)
     });
 
 }
